e3f789aff1c174d80859511c5ffe4d67ba0119ea
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-01 15:26:30 -0400

    Set up MongoDB.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+app.get("/products", (req, res) => {});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(1234, () => {
   console.log("Server started at http://localhost:1234");
