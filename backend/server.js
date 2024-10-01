501eb548e5ef9e88841cbf286096cfc136ebc3e3
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-01 15:46:51 -0400

    Connected Mongo DB and adding models now.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -10,5 +11,7 @@ app.get("/products", (req, res) => {});
 console.log(process.env.MONGO_URI);
 
 app.listen(1234, () => {
+  connectDB();
+
   console.log("Server started at http://localhost:1234");
 });
